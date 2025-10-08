a5ce1718aba89f9ba36833fffa97faaaa01862b4
Sam McLamb <131833559+Mrinfallable@users.noreply.github.com>
2025-10-08 15:51:20 -0400

    Create find-string-recursive.ps1

---
+++ find-string-recursive.ps1
@@ -0,0 +1,12 @@
+param(
+[string]$Path=".",
+[string]$String,
+[string]$OutPut="none"
+)
+
+if($OutPut == "none"){
+Get-ChildItem $Path -Recurse -Directory | ForEach-Object {$path = $_.FullName; findstr /i "$String" "$path\*"}
+}
+else{
+Get-ChildItem $Path -Recurse -Directory | ForEach-Object {$path = $_.FullName; findstr /i "$String" "$path\*"} | Out-File -File $OutPut
+}
