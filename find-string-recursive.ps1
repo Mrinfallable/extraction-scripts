bc4d4a36cac4101ffe2bd6e64dde0b5c25e3a202
Sam McLamb <131833559+Mrinfallable@users.noreply.github.com>
2026-02-10 13:38:40 -0500

    Update find-string-recursive.ps1

--- find-string-recursive.ps1
+++ find-string-recursive.ps1
@@ -1,10 +1,11 @@
 param(
-[string]$Path=".",
-[string]$String,
-[string]$Output=New-TemporaryFile | Select-Object -Property Name
-[string]$Type="*.*"
+[string]$Path = ".",
+[string]$String = "*",
+[string]$Output = New-TemporaryFile | Select-Object -Property Name,
+[string]$Type = "*.*"
 )
 
+$dirs=Get-ChildItem
 Get-ChildItem $Path -Recurse -Directory | findstr /i "$Type | ForEach-Object {$path = $_.FullName; findstr /i "$String" "$path\*"} | Out-File -File $Output
 
 Write-Output $Output
