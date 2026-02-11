ad40737b600779ddc156fc4aceae41b5933b4aff
Sam McLamb <131833559+Mrinfallable@users.noreply.github.com>
2026-02-10 21:43:35 -0500

    Update find-string-recursive.ps1

--- find-string-recursive.ps1
+++ find-string-recursive.ps1
@@ -6,15 +6,12 @@ param(
 [string]$Len = ($String.length * 5)
 )
 
-
 Get-ChildItem $Path -Recurse -Include "*.$Type" | Select-Object -Property "FullName" | ForEach-Object {
     $path = $_.FullName
     $matchingPath = (findstr /i /m /c:"$String" "$path")
     echo $matchingPath
     if ($null -ne $matchingPath){
         $content = (Get-Content $matchingPath -Raw)
-        #echo $content
         echo ($content.Substring($content.IndexOf($String), $len))
     }
-
 }
